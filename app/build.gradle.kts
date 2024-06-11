ac35b49fe8665b053f351db4913a985f76efd81f
jromarph <137426535+jromarph@users.noreply.github.com>
2024-06-12 01:19:01 +0800

    database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(fileTree(mapOf(
+        "dir" to "D:\\Download\\jtds1.3.1",
+        "include" to listOf("*.aar", "*.jar"),
+    )))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
