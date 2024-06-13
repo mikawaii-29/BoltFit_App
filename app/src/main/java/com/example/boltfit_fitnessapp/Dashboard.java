d5e780687f2b1c62f33db498c0e03af1d9fe8601
jromarph <137426535+jromarph@users.noreply.github.com>
2024-06-14 01:49:23 +0800

    changes in dashboard and database heler and mainact

--- app/src/main/java/com/example/boltfit_fitnessapp/Dashboard.java
+++ app/src/main/java/com/example/boltfit_fitnessapp/Dashboard.java
@@ -46,6 +46,7 @@ public class Dashboard extends AppCompatActivity {
         if (username != null) {
             textView10.setText("Hi " + username + "!");
         }
+        displayUserDetails();
     }
 
     private void initializeViews() {
@@ -74,7 +75,6 @@ public class Dashboard extends AppCompatActivity {
         View headerView = navigationView.getHeaderView(0);
         name = headerView.findViewById(R.id.nameText1);
         email = headerView.findViewById(R.id.emailText1);
-        displayUserDetails();
         menuBtn.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
     }
 
